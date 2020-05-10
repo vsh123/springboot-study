55ef76ca36ed56f7fc9051ccb400ba7978851fb1
SunHo Van <vsh123@naver.com>
2020-05-10 21:06:07 +0900

    logback-step2

--- src/main/java/roadmap/logback/api/LogBackApiController.java
+++ src/main/java/roadmap/logback/api/LogBackApiController.java
@@ -1,5 +1,7 @@
 package roadmap.logback.api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/roadmap/logback")
 public class LogBackApiController {
 
+    private static final Logger roadmapConsoleLogger = LoggerFactory.getLogger("roadmapConsole");
+
     @GetMapping
     public String logbackTest() {
+        roadmapConsoleLogger.info("콘솔 로깅 입니다.");
         return "안녕하세요";
     }
 }
